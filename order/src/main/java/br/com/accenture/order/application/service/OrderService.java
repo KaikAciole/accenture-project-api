d83ccc4c19472aaf470d22b85a9136d88a8faf0a
KaikAciole <kaikaciole1.0@gmail.com>
2026-05-07 14:21:31 -0300

    feat: cria camada de application/service para order

---
+++ order/src/main/java/br/com/accenture/order/application/service/OrderService.java
@@ -0,0 +1,54 @@
+package br.com.accenture.order.application.service;
+
+import br.com.accenture.order.application.dto.OrderItemCommand;
+import br.com.accenture.order.domain.exception.OrderNotFoundException;
+import br.com.accenture.order.domain.model.Order;
+import br.com.accenture.order.domain.model.OrderItem;
+import br.com.accenture.order.domain.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @Transactional
+    public Order createOrder(String customerId, List<OrderItemCommand> itemsCommand) {
+
+        Order newOrder = Order.createNew(customerId);
+
+        for (OrderItemCommand cmd : itemsCommand) {
+            OrderItem item = OrderItem.createNew(cmd.sku(), cmd.quantity(), cmd.unitPrice());
+            newOrder.addItem(item);
+        }
+
+        return orderRepository.save(newOrder);
+    }
+
+    @Transactional(readOnly = true)
+    public Order findById(UUID id) {
+        return orderRepository.findById(id)
+                .orElseThrow(() -> new OrderNotFoundException(id));
+    }
+
+    @Transactional(readOnly = true)
+    public List<Order> findByCustomerId(String customerId) {
+        return orderRepository.findByCustomerId(customerId);
+    }
+
+    @Transactional
+    public void confirmOrderReservation(UUID id) {
+        Order order = findById(id);
+        order.confirmReservation();
+
+        orderRepository.save(order);
+    }
+}

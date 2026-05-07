de9b40440ac69c78390d7c38ace11822c0d4a97b
KaikAciole <kaikaciole1.0@gmail.com>
2026-05-07 13:18:29 -0300

    feat: corrige implementacao das entidade e interfaces no dominio

--- order/src/main/java/br/com/accenture/order/domain/enums/OrderStatus.java
+++ order/src/main/java/br/com/accenture/order/domain/enums/OrderStatus.java
@@ -1,10 +1,5 @@
 package br.com.accenture.order.domain.enums;
 
 public enum OrderStatus {
-    PENDING,
-    ANALYZING_FRAUD,
-    RESERVED,
-    PAID,
-    FAILED,
-    CANCELED
+    PENDING, ANALYZING_FRAUD, RESERVED, PAID, FAILED, CANCELED
 }
---
+++ order/src/main/java/br/com/accenture/order/domain/repository/OrderRepository.java
@@ -0,0 +1,19 @@
+package br.com.accenture.order.domain.repository;
+
+import br.com.accenture.order.domain.model.Order;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface OrderRepository {
+
+    Order save(Order order);
+
+    Optional<Order> findById(UUID id);
+
+    List<Order> findByCustomerId(String customerId);
+
+    void deleteById(UUID id);
+
+}

5a39b0cb4f8213eb8863292154823476d94a14fa
KaikAciole <kaikaciole1.0@gmail.com>
2026-05-08 21:02:56 -0300

    feat: adiciona o projeto de autenticacao no repositorio

---
+++ auth/src/test/java/br/com/accenture/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.accenture.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

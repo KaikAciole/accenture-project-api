60a03700779a6877dae064d555f1ccbde3c47c2a
luanrrsouza <luanrrdsouza@gmail.com>
2026-05-08 21:49:45 -0300

    cria modelagem da entidade payment

---
+++ payment/src/main/java/br/com/accenture/payment/domain/exception/PaymentNotFoundException.java
@@ -0,0 +1,18 @@
+package br.com.accenture.payment.domain.exception;
+
+import java.util.UUID;
+
+public class PaymentNotFoundException extends RuntimeException {
+
+    public PaymentNotFoundException(UUID id) {
+        super("Payment not found with id: " + id);
+    }
+
+    public static PaymentNotFoundException byOrderId(UUID orderId) {
+        return new PaymentNotFoundException("Payment not found with order id: " + orderId);
+    }
+
+    private PaymentNotFoundException(String message) {
+        super(message);
+    }
+}

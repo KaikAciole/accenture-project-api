f7aae9f8e2d68ff19453981e06dc632d1e40a1ea
LiedsonAugusto <liedsonaugusto@gmail.com>
2026-05-11 17:34:01 -0300

    chore: conserto do jacskonJsonConverter para a versao correta

--- customer/src/main/java/br/com/accenture/customer/infrastructure/messaging/RabbitMqConfig.java
+++ customer/src/main/java/br/com/accenture/customer/infrastructure/messaging/RabbitMqConfig.java
@@ -3,7 +3,7 @@ package br.com.accenture.customer.infrastructure.messaging;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,7 +22,7 @@ public class RabbitMqConfig {
 
     @Bean
     public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
+        return new JacksonJsonMessageConverter();
     }
 
     @Bean

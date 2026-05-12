5d41b54ae62c8f092185bdd878d6769bc58a4abf
Liedson Augusto <93728229+LiedsonAugusto@users.noreply.github.com>
2026-05-12 16:04:48 -0300

    merge: 96-criando-assistant-service na main

---
+++ assistent/src/main/java/br/com/accenture/assistent/AssistentApplication.java
@@ -0,0 +1,13 @@
+package br.com.accenture.assistent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistentApplication.class, args);
+	}
+
+}
---
+++ assistent/src/test/java/br/com/accenture/assistent/AssistentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.accenture.assistent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

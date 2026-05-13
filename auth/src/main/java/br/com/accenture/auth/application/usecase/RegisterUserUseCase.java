c33828030e4f5f8d531ca9b0989a96c95371e5ac
Kaik Aciole <143836390+KaikAciole@users.noreply.github.com>
2026-05-12 22:12:23 -0300

    [FEATURE] - Adicionar integração com mensageria no microserviço Auth (#107)
    * feat: adiciona integracao com mensageria no servico de auth
    
    * chore: remove gitkeep de pastas

--- auth/src/main/java/br/com/accenture/auth/application/usecase/RegisterUserUseCase.java
+++ auth/src/main/java/br/com/accenture/auth/application/usecase/RegisterUserUseCase.java
@@ -7,19 +7,24 @@ import br.com.accenture.auth.domain.model.UserCredential;
 import br.com.accenture.auth.domain.repository.UserCredentialRepository;
 import br.com.accenture.auth.domain.service.PasswordEncoder;
 import br.com.accenture.auth.domain.vo.Email;
+import br.com.accenture.auth.infrastructure.messaging.RabbitMQConfig;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RegisterUserUseCase {
 
     private final UserCredentialRepository repository;
     private final PasswordEncoder passwordEncoder;
+    private final RabbitTemplate rabbitTemplate;
 
     @Transactional
     public void execute(RegisterCommand command) {
@@ -43,5 +48,16 @@ public class RegisterUserUseCase {
         );
 
         repository.save(newCredential);
+
+        String mensagemJson = String.format("{\"customerId\": \"%s\", \"email\": \"%s\"}",
+                newCredential.getCustomerId(), newCredential.getEmail().value());
+
+        rabbitTemplate.convertAndSend(
+                RabbitMQConfig.EXCHANGE_NAME,
+                RabbitMQConfig.ROUTING_KEY,
+                mensagemJson
+        );
+
+        log.info("Mensagem enviada com sucesso ao RabbitMQ! Novo usuario: {}", newCredential.getEmail().value());
     }
 }

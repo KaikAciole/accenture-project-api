b3890996eb5ab954500fec15e8e14b8b9d71e409
KaikAciole <kaikaciole1.0@gmail.com>
2026-05-13 11:02:49 -0300

    feat: configuracao do rabbitmq e publicador de eventos

---
+++ order/src/main/java/br/com/accenture/order/application/dto/event/OrderCanceledEvent.java
@@ -0,0 +1,9 @@
+package br.com.accenture.order.application.dto.event;
+
+import java.util.UUID;
+
+public record OrderCanceledEvent(
+        UUID orderId,
+        String customerId,
+        String reason
+) {}

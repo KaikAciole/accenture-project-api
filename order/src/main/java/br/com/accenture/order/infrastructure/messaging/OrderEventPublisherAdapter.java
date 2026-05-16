a6229a3f14d2023775ab2e62992f37729b6e149b
LiedsonAugusto <liedsonaugusto@gmail.com>
2026-05-16 14:31:27 -0300

    chore: publishOrderReservedEvent correto

--- order/src/main/java/br/com/accenture/order/infrastructure/messaging/OrderEventPublisherAdapter.java
+++ order/src/main/java/br/com/accenture/order/infrastructure/messaging/OrderEventPublisherAdapter.java
@@ -4,6 +4,7 @@ import br.com.accenture.order.application.dto.event.OrderCanceledEvent;
 import br.com.accenture.order.application.dto.event.OrderCreatedEvent;
 import br.com.accenture.order.application.dto.event.OrderPaidEvent;
 import br.com.accenture.order.application.dto.event.OrderRefundedEvent;
+import br.com.accenture.order.application.dto.event.OrderReservedEvent;
 import br.com.accenture.order.application.publisher.OrderEventPublisher;
 import br.com.accenture.order.domain.model.Order;
 import br.com.accenture.order.infrastructure.persistence.OutboxEventRepository;
@@ -49,17 +50,8 @@ public class OrderEventPublisherAdapter implements OrderEventPublisher {
 
     @Override
     public void publishOrderReservedEvent(Order order) {
-        try {
-            var event = new br.com.accenture.order.application.dto.event.OrderCreatedEvent(
-                    order.getId(),
-                    order.getCustomerId(),
-                    order.getTotalAmount(),
-                    java.util.Collections.emptyList()
-            );
-            saveToOutbox("Order", order.getId().toString(), "order.reserved", event);
-        } catch (Exception e) {
-            throw new RuntimeException("Falha ao publicar evento order.reserved", e);
-        }
+        var event = new OrderReservedEvent(order.getId(), order.getCustomerId());
+        saveToOutbox("Order", order.getId().toString(), "order.reserved", event);
     }
 
     @Override
